b0568c256ef3a145aa375ff6539b26775bd3a39e
Kashif Ahmad <19bcs2983@gmail.com>
2025-01-23 19:43:25 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Ensures compatibility with Render's dynamic port
+    port: process.env.PORT || 3000,  // Use process.env.PORT for dynamic port or default to 3000 for local dev
+    host: '0.0.0.0',                // Ensure the server is accessible on all IPs
   },
 });
